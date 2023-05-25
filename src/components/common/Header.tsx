80f8c4e8d6d258c1997488f5e3a5dbd7c896b9a8
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-05-25 22:27:15 +0200

    Added bg background, changed nav structure and added linear gradient. Created header section.

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -7,7 +7,10 @@ type NavLinkProps = {
 
 function NavLink({ href, children }: NavLinkProps) {
   return (
-    <a href={href} className="inline-flex text-white transition-colors hover:text-primary-200">
+    <a
+      href={href}
+      className="inline-flex text-white transition-colors hover:text-primary-200"
+    >
       {children}
     </a>
   );
@@ -15,29 +18,32 @@ function NavLink({ href, children }: NavLinkProps) {
 
 export default function Header() {
   return (
-    <nav className="bg-primary-500">
+    <nav className="bg-nav-gradient opacity-90">
       <div className="mx-auto flex w-11/12 max-w-screen-xl items-center justify-between py-4">
         <a href="/" className="inline-flex" title="Domov" aria-label="Domov">
-          <img src="/photos/logo/borstnikovanje-logo-white-no-text.svg" width={40} height={40} alt="Borštnikovanje logo" />
+          <img
+            src="/photos/logo/borstnikovanje-logo-white-no-text.svg"
+            width={40}
+            height={40}
+            alt="Borštnikovanje logo"
+          />
         </a>
 
-        <div className="flex items-center gap-6">
-          <div className="hidden items-center gap-6 text-white lg:flex">
-            <NavLink href="/program">Program</NavLink>
+        <div className="hidden items-center gap-14 text-white lg:flex">
+          <NavLink href="/program">Program</NavLink>
 
-            <NavLink href="/delavnice">Delavnice</NavLink>
+          <NavLink href="/delavnice">Delavnice</NavLink>
 
-            <NavLink href="/kdo-smo">Kdo smo</NavLink>
+          <NavLink href="/kdo-smo">Kdo smo</NavLink>
 
-            <NavLink href="/ali-ves">Ali veš?</NavLink>
+          <NavLink href="/ali-ves">Ali veš?</NavLink>
 
-            <NavLink href="/galerija">Galerija</NavLink>
-          </div>
-
-          <Button intent="white" size="sm">
-            Nakup kart
-          </Button>
+          <NavLink href="/galerija">Galerija</NavLink>
         </div>
+
+        <Button intent="white" size="sm">
+          Nakup kart
+        </Button>
       </div>
     </nav>
   );
