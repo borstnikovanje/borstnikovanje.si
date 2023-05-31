fdd90ab5279dd2950bb83d16fcdd918871680008
Andrijan Tasevski <andrijantasevski@gmail.com>
2023-05-31 20:32:13 +0200

    Working version of linktree.

--- www/src/components/common/NewsletterForm.tsx
+++ www/src/components/common/NewsletterForm.tsx
@@ -1,6 +1,8 @@
 import { IconChevronRight } from "@tabler/icons-react";
 import { useState } from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
+import { Toaster, toast } from "react-hot-toast";
+import LoadingSpinner from "../ui/LoadingSpinner";
 
 type NewsletterForm = {
   email: string;
@@ -10,6 +12,7 @@ type FetchStatus = "idle" | "fetching";
 
 export default function NewsletterForm() {
   const [status, setStatus] = useState<FetchStatus>("idle");
+
   const {
     register,
     handleSubmit,
@@ -30,31 +33,39 @@ export default function NewsletterForm() {
 
       setStatus("idle");
       reset();
-    } catch (error) {}
+    } catch (error) {
+      toast.error("There was an error!");
+      setStatus("idle");
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
-      <div className="flex w-full items-center">
-        <div className="w-full">
-          <label htmlFor="emailInput" className="sr-only">
-            E-mail
-          </label>
-          <input
-            {...register("email", { required: true })}
-            className="w-full border border-primary-foreground bg-transparent p-2 placeholder:text-neutral-600"
-            type="email"
-            placeholder="ime.priimek@gmail.com"
-            id="emailInput"
-          />
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+        <div className="flex w-full items-center">
+          <div className="w-full">
+            <label htmlFor="emailInput" className="sr-only">
+              E-mail
+            </label>
+            <input
+              {...register("email", { required: true })}
+              className="w-full border border-primary-foreground bg-transparent p-2 placeholder:text-neutral-600"
+              type="email"
+              placeholder="ime.priimek@gmail.com"
+              id="emailInput"
+            />
+          </div>
+
+          <button className="flex h-full items-center border border-transparent bg-primary-foreground p-2 text-primary">
+            {status === "idle" && <IconChevronRight className="text-primary" />}
+            {status === "fetching" && <LoadingSpinner />}
+          </button>
         </div>
 
-        <button className="flex items-center border border-transparent bg-primary-foreground p-2 text-primary">
-          <IconChevronRight className="text-primary" />
-        </button>
-      </div>
+        {errors.email && <p className="text-red-600">Vnesite vaš email.</p>}
+      </form>
 
-      {errors.email && <p className="text-red-500">Vnesite vaš email.</p>}
-    </form>
+      <Toaster position="top-right" />
+    </>
   );
 }
